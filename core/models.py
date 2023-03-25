fc14a7c135a78d2ca92c989989c6c1f0cd3c5651
JoseDiazResendez <al180247@alumnos.uacj.mx>
2023-03-25 17:52:01 -0600

    Post y Upload

--- core/models.py
+++ core/models.py
@@ -1,4 +1,9 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+import uuid
+from datetime import datetime
+
+User = get_user_model()
 
 # Create your models here.
 class Usuarios(models.Model):
@@ -6,4 +11,24 @@ class Usuarios(models.Model):
     lastname = models.CharField(max_length=100)
     nickname = models.CharField(max_length=100)
     email = models.CharField(max_length=100)
+
+class Perfil(models.Model):
+    usuario = models.ForeignKey(User, on_delete = models.CASCADE)
+    id_usuario = models.IntegerField()
+    bio = models.TextField(blank = True)
+    profileimg = models.ImageField(upload_to = 'profile_images', default = 'https://static.vecteezy.com/system/resources/previews/005/544/718/original/profile-icon-design-free-vector.jpg')
     
+    def __str__(self):
+        return self.usuario.get_username
+    
+class Post(models.Model):
+    id = models.UUIDField(primary_key = True, default = uuid.uuid4)
+    usuario = models.CharField(max_length = 100)
+    image = models.ImageField(upload_to = 'post_images')
+    caption = models.TextField()
+    created_at = models.DateTimeField(default = datetime.now)
+    number_likes = models.IntegerField(default = 0)
+
+    def __str__(self):
+        return self.usuario 
+
--- core/views.py
+++ core/views.py
@@ -3,12 +3,28 @@ from django.http import HttpResponse
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages, auth
+from .models import Perfil, Post
 
-#HOLA FER, COMO ANDAS?
 # Create your views here.
 @login_required(login_url='signin')
 def index(request):
-    return render(request,'index.html')
+    user_object = User.objects.get(username = request.user.username)
+    user_perfil = Perfil.objects.get(user = user_object)
+    return render(request,'index.html', {'user_perfil': user_perfil})
+
+@login_required(login_url='signin')
+def upload(request):
+    if request.method == 'POST':
+        user = request.user.username
+        image = request.FILES.get('image_upload')
+        caption = request.POST['caption']
+
+        new_post = Post.objects.create(user = usuario, image = image, caption = caption)
+        new_post.save()
+
+        return redirect('/')
+    else:
+        return redirect('/')
 
 def signup(request):
 
