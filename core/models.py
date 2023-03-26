06bdfe7f5cf5d690b22ec3949c5b5825ee1081ac
JoseDiazResendez <al180247@alumnos.uacj.mx>
2023-03-25 18:46:05 -0600

    modificacion

--- core/models.py
+++ core/models.py
@@ -13,22 +13,22 @@ class Usuarios(models.Model):
     email = models.CharField(max_length=100)
 
 class Perfil(models.Model):
-    usuario = models.ForeignKey(User, on_delete = models.CASCADE)
-    id_usuario = models.IntegerField()
+    user = models.ForeignKey(User, on_delete = models.CASCADE)
+    id_user = models.IntegerField()
     bio = models.TextField(blank = True)
-    profileimg = models.ImageField(upload_to = 'profile_images', default = 'https://static.vecteezy.com/system/resources/previews/005/544/718/original/profile-icon-design-free-vector.jpg')
+    profileimg = models.ImageField(upload_to = 'profile_images', default ='bpp.png')
     
     def __str__(self):
-        return self.usuario.get_username
+        return self.user.username
     
 class Post(models.Model):
     id = models.UUIDField(primary_key = True, default = uuid.uuid4)
-    usuario = models.CharField(max_length = 100)
+    user = models.CharField(max_length = 100)
     image = models.ImageField(upload_to = 'post_images')
     caption = models.TextField()
     created_at = models.DateTimeField(default = datetime.now)
     number_likes = models.IntegerField(default = 0)
 
     def __str__(self):
-        return self.usuario 
+        return self.user
 
